3edb52e917f8fec1f62947927429dd2754986a55
Toni Marti Albons <toni.m.a.2003@gmail.com>
2025-01-26 23:26:21 +0100

    Under development

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<script>
-</script>
+<!--
+    CV DONWLOAD LINK https://nextcloud.tonimartal.com/index.php/s/wcCzsLwdKkbZtnK/download/CV-ToniMartiAlbons.pdf
+-->
+<title>Toni's portfolio</title>
 
-<h1 class="text-blue-700">This is a test</h1>
+<article class="m-auto pl-4 pr-4 max-w-80">
+    <header>
+        <h1>Toni Martí Albons</h1>
+        <p>Multi-Disciplined Developer</p>
+    </header>
+    
+    <p>Page currently under development.</p>
+</article>
