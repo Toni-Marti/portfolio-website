fc50295f929d4032c14f91d1170f977bfeb9034f
Toni Martí <toni.m.a.2003@gmail.com>
2025-04-22 19:46:55 +0200

    Fixed typo and improved wording.

--- src/lib/cards_content.ts
+++ src/lib/cards_content.ts
@@ -13,7 +13,7 @@ export const cards_content: Array<ProjectInfo> = [
     title: "Nextcloud Server",
     description: [
       "This project involves the deployment and maintenance of a self-hosted file storing server. Nextcloud is an open source alternative to things like Google Drive or Dropbox.",
-      "First tried to set it up when right when I started university, so it was way before I has sufficient knowledge to do it. But after bashing my head against the wall for a while I managed to get it working. Since then, it was presented me with a hole bunch of unsuspected challenges any server administrator would face.",
+      "First tried to set it up right when I started university, so it was way before I had sufficient knowledge to do so. Since then, it was presented me with a hole bunch of unsuspected challenges, as any server administrator would face.",
       "One of those challenges came up because the network I have the server running on doesn't dispose of a static IP, so the model would become unreachable every time the IP changed. To solve this I created a script that runs every couple minutes, fetches the server's IP from the web and if it detects a change it then uses the API provided by the domain name registrar use to update the DNS records; all while sending me an email with a log of the process.",
       "On nextcloud.tonimartal.com you can see it's login screen.",
     ],
